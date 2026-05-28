8792ff6a1998c502abaa5671b3b347f31d7644dc
Harry Taylor <ht555@exeter.ac.uk>
2026-05-28 22:29:46 +0100

    changed file contents and gitignore

--- simulator.py
+++ simulator.py
@@ -4,7 +4,6 @@ from button import Button
 import pickle
 import time
 import os
-from binaryConvert import binaryConvert
 
 #clock pulse variables set
 finalTime = 0
@@ -37,6 +36,24 @@ RED = "#AE0E2E"
 #930E29
 GREEN = "#7CA982"
 GREY = "#967D69"
+
+#number is passed in from main logic
+def binaryConvert(number):
+    binaryText = ""
+    reverseBinaryList = []
+    #denary number zero produces zero in binary, so zero is returned
+    if number == 0:
+        return 0
+    #number is continually divided by two to generate binary bits 
+    while number != 0:
+        numberRemainder = number%2
+        reverseBinaryList.append(numberRemainder)
+        number = number//2
+    #list is converted to string
+    for i in range(len(reverseBinaryList)-1,-1, -1):
+        binaryText = binaryText + str(reverseBinaryList[i])
+    return binaryText
+
 
 def loadFontFromFolder(filename, size):
     base_dir = os.path.dirname(os.path.abspath(__file__))
